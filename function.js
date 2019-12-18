6bfd0bf34476a760d5ec342129a73ca9a772bf91
John Jones Jr <jayjohnjones@gmail.com>
2019-12-17 19:15:02 -0500

    add html, js, css

---
+++ function.js
@@ -0,0 +1,6 @@
+// Today we will create a simple page that loads gifs. We will be doing this as a class, so as part of the WARMUP.
+// WARMUP:
+// 1. I want you to create a directory(folder) with a JavaScript file, a HTML file, and CSS file.
+// 2. The <body> tag should contain a <form> tag, and a <div>tag with a class of “content”
+// 3. the <form> tag should contain an <input> tag with type=text, a select tag with a disabled <option> tag and another input tag with the type=submit
+// 2. Create an account on giphy 
